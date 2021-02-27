28b1304488cf99cb50302f2a84ad8de2b972def2
Iresh Dudeja <dudeja.iresh@gmail.com>
2021-02-27 20:09:31 +0100

    Car Item Component

--- TeslaClone/App.js
+++ TeslaClone/App.js
@@ -7,12 +7,7 @@
  */
 
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {StyleSheet, View, Text, StatusBar} from 'react-native';
 import CarItem from './components/CarItem';
 
 import {
@@ -29,14 +24,35 @@ const App = () => {
       <StatusBar barStyle="dark-content" />
 
       <View>
-        <CarItem></CarItem>
+        <CarItem
+          name={'Model S'}
+          tagLine={'Order Online for'}
+          tagLineCTA={'Touchless Delivery'}
+          image={require('./assets/images/ModelS.jpeg')}
+        />
+
+        {/* <CarItem
+          name={'Model 3'}
+          tagLine={'Order Online for Touchless Delivery'}
+          image={require('./assets/images/Model3.jpeg')}
+        />
+
+        <CarItem
+          name={'Model X'}
+          tagLine={'Order Online for Touchless Delivery'}
+          image={require('./assets/images/ModelX.jpeg')}
+        />
+
+        <CarItem
+          name={'Model Y'}
+          tagLine={'Order Online for Touchless Delivery'}
+          image={require('./assets/images/ModelY.jpeg')}
+        /> */}
       </View>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
 
 export default App;
--- TeslaClone/components/CarItem/index.js
+++ TeslaClone/components/CarItem/index.js
@@ -3,24 +3,39 @@ import {View, Text, ImageBackground} from 'react-native';
 import StyledButton from '../StyledButton';
 import styles from './styles';
 
-const CarItem = () => {
-    return (
-        <View style={styles.carContainer}>
-          <ImageBackground
-            source={require('../../assets/images/ModelS.jpeg')}
-            style={styles.image}
-          />
-          <View style={styles.titles}>
-            <Text style={styles.mainTitle}> Model S</Text>
-            <Text style={styles.subTitle}> Starting at $69,420</Text>
-          </View>
-          <StyledButton type={'primary'} text={'Custom Order'} 
-          onPress={()=>{console.warn('custom order pressed')}}/>
-          <StyledButton type={'secondary'} text={'Existing Inventory'}
-              onPress={()=>{console.warn('inventory order pressed')}}
-          />
-        </View>
-    )
-}
+const CarItem = (props) => {
+  const {name, tagLine, tagLineCTA, image} = props;
+
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground source={image} style={styles.image} />
+
+      <View style={styles.titles}>
+        <Text style={styles.mainTitle}> {name}</Text>
+        <Text style={styles.subTitle}>
+          {tagLine} <Text style={styles.subTitleCTA}>{tagLineCTA}</Text>
+        </Text>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <StyledButton
+          type={'primary'}
+          text={'Custom Order'}
+          onPress={() => {
+            console.warn('custom order pressed');
+          }}
+        />
+
+        <StyledButton
+          type={'secondary'}
+          text={'Existing Inventory'}
+          onPress={() => {
+            console.warn('inventory order pressed');
+          }}
+        />
+      </View>
+    </View>
+  );
+};
 
 export default CarItem;
